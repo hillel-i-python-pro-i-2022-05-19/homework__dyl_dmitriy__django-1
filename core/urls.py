2ebc43a47944e8c726f756a465b22002655d4e98
DylDmitriy <dyl.dmitriy@gmail.com>
2022-08-27 21:34:39 +0300

    HM21.3

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("apps.phone_book.urls")),
-    path("session", include("apps.sessions_example.urls")),
+    path("", include("apps.base.urls")),
+    path("user-generator/", include("apps.user_generator.urls")),
+    path("caesar/", include("apps.caesar.urls")),
+    path("contacts/", include("apps.contacts.urls")),
+    path("sessions/", include("apps.session_storage.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("users/", include("apps.users.urls")),
+    path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
